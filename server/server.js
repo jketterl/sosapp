39ed40aa1d6fb0de2378c72f163c8c478b34bac2
Jakob Ketterl <webmaster@tracklistings.de>
2014-07-10 21:00:42 +0000

    add some bootstrap and a download link

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var routes = require('./routes');
 
 var app = express();
+app.use('/assets', express.static(__dirname + '/assets'));
 routes(app);
 app.listen(3000);
 
